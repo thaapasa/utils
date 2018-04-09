a288114d4c11a299969530947a733b4cd58f0089
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2018-04-09 12:51:26 +0300

    Fix Either + Optional to have better typings

--- src/kotlin/fi/haapatalo/utils/Either.kt
+++ src/kotlin/fi/haapatalo/utils/Either.kt
@@ -24,13 +24,13 @@ sealed class Either<out L, out R> {
 
     inline fun <X> mapLeft(mapper: (L) -> X): Either<X, R> = when (this) {
         is Left -> Left(mapper(left))
-        is Right -> this as Right<X, R>
+        is Right -> this
     }
     inline fun <X> mapRight(mapper: (R) -> X): Either<L, X> = when (this) {
-        is Left -> this as Left<L, X>
+        is Left -> this
         is Right -> Right(mapper(right))
     }
 }
 
-class Left<out L, out R>(val left: L): Either<L, R>()
-class Right<out L, out R>(val right: R): Either<L, R>()
+class Left<out L>(val left: L): Either<L, Nothing>()
+class Right<out R>(val right: R): Either<Nothing, R>()
--- src/kotlin/fi/haapatalo/utils/Optional.kt
+++ src/kotlin/fi/haapatalo/utils/Optional.kt
@@ -1,17 +1,9 @@
 package fi.haapatalo.utils
 
-sealed class Optional<T> {
+sealed class Optional<out T> {
     fun get(): T = when (this) {
         is Some -> value
         is None -> throw IllegalStateException("None.get() is not allowed")
-    }
-    infix fun or(x: T): T = when (this) {
-        is Some -> value
-        is None -> x
-    }
-    inline fun orGet(x: () -> T): T = when (this) {
-        is Some -> value
-        is None -> x()
     }
     fun orNull() = when (this) {
         is Some -> value
@@ -19,21 +11,29 @@ sealed class Optional<T> {
     }
     inline fun <X> map(mapper: (T) -> X): Optional<X> = when (this) {
         is Some -> Some(mapper(value))
-        is None -> this as Optional<X>
+        is None -> this
     }
     inline fun <X> flatMap(mapper: (T) -> Optional<X>): Optional<X> = when (this) {
         is Some -> mapper(value)
-        is None -> this as Optional<X>
+        is None -> this
     }
     companion object {
         fun <T> of(t: T?): Optional<T> = when (t) {
-            null -> None.of()
+            null -> None
             else -> Some(t)
         }
     }
 }
 
-object None: Optional<Any>() {
-    fun <T> of(): Optional<T> = this as Optional<T>
+object None: Optional<Nothing>()
+data class Some<out T>(val value: T): Optional<T>()
+
+infix fun <T> Optional<T>.or(x: T): T = when (this) {
+    is Some -> value
+    is None -> x
 }
-data class Some<T>(val value: T): Optional<T>()
+
+inline fun <T> Optional<T>.orGet(x: () -> T): T = when (this) {
+    is Some -> value
+    is None -> x()
+}
